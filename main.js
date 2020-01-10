8ca94103e53cda4da801d8e967dfff429f942cf0
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2020-01-10 14:51:18 +0100

    Bottoni gestiti con Disabled

--- main.js
+++ main.js
@@ -10,29 +10,21 @@ month_Generator(starting_moment);
 checking_Holidays(starting_moment);
 
 $('#month_next').click(function(){
-    if ($(this).hasClass('deactivate')) {
-        alert('Questo calendario copre solo l\'anno 2018');
-    } else {
-        $('#month_section').empty();
+    $('#month_section').empty();
 
-        starting_moment.add(1, 'months');
+    starting_moment.add(1, 'months');
 
-        month_Generator(starting_moment);
-        checking_Holidays(starting_moment);
-    }
+    month_Generator(starting_moment);
+    checking_Holidays(starting_moment);
 });
 
 $('#month_prev').click(function(){
-    if ($(this).hasClass('deactivate')) {
-        alert('Questo calendario copre solo l\'anno 2018');
-    } else {
-        $('#month_section').empty();
+    $('#month_section').empty();
 
-        starting_moment.subtract(1, 'months');
+    starting_moment.subtract(1, 'months');
 
-        month_Generator(starting_moment);
-        checking_Holidays(starting_moment);
-    }
+    month_Generator(starting_moment);
+    checking_Holidays(starting_moment);
 });
 
 
@@ -85,12 +77,16 @@ function checking_Holidays(date) {
 };
 
 function checking_valid_month(num_of_month) {
-    $('#month_prev').removeClass('deactivate');
-    $('#month_next').removeClass('deactivate');
+    // $('#month_prev').removeClass('deactivate');
+    // $('#month_next').removeClass('deactivate');
+    $('#month_prev').prop('disabled', false);
+    $('#month_next').prop('disabled', false);
     if (num_of_month == 0) {
-        $('#month_prev').addClass('deactivate');
+        // $('#month_prev').addClass('deactivate');
+        $('#month_prev').prop('disabled', true);
     } else if (num_of_month == 11) {
-        $('#month_next').addClass('deactivate');
+        // $('#month_next').addClass('deactivate');
+        $('#month_next').prop('disabled', true)
     }
 };
 
